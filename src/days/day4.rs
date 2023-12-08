a2164a4c5cafe4709ffb8fea5ba69d5309822bf8
broccoli <me@broccol.ai>
2023-12-08 19:49:21 +0000

    day(four): minor improvements

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -2,9 +2,7 @@ use std::collections::{HashSet};
 use yaah::aoc;
 use crate::utilities::split_once_and_trim;
 
-#[derive(Debug)]
 struct Card {
-    identifier: u32,
     winning_numbers: HashSet<u8>,
     owned_numbers: HashSet<u8>
 }
@@ -18,13 +16,10 @@ fn day4_part1(input: &'static str) -> usize {
 }
 
 fn parse_into_card(input: &str) -> Card {
-    let (raw_day, raw_numbers) =  split_once_and_trim(input, ':').unwrap();
+    let (_, raw_numbers) =  split_once_and_trim(input, ':').unwrap();
     let (raw_winning, raw_owned) = split_once_and_trim(raw_numbers, '|').unwrap();
 
-    let day = raw_day.trim_start_matches("Card").trim().parse().unwrap();
-
     Card {
-        identifier: day,
         winning_numbers: parse_into_num_array(raw_winning),
         owned_numbers: parse_into_num_array(raw_owned)
     }
@@ -53,23 +48,18 @@ fn day4_part2(input: &'static str) -> u32 {
 }
 
 fn calculate_amount_of_cards(cards: &[Card]) -> u32 {
-    const fn card_as_index(card: &Card) -> usize { (card.identifier - 1) as usize }
-
     let mut card_amounts: Vec<u32> = vec![1; cards.len()];
 
-    for card in cards {
-        let card_index = card_as_index(card);
+    for (card_index, card) in cards.iter().enumerate() {
         let amount_of_card = card_amounts[card_index];
 
         let score = winning_intersection_size(card);
         let next_index = card_index + 1;
 
         for index in next_index..next_index + score {
-            let Some(amount) = card_amounts.get_mut(index) else {
-                continue
+            if let Some(amount) = card_amounts.get_mut(index) {
+                *amount += amount_of_card;
             };
-
-            *amount += amount_of_card;
         }
     }
 
