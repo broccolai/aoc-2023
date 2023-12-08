eeb64d2c876872621eb4623db688f51907c5d58d
broccoli <me@broccol.ai>
2023-12-08 19:33:53 +0000

    day(four): part two working

--- src/days/day4.rs
+++ src/days/day4.rs
@@ -1,45 +1,83 @@
-use std::collections::HashSet;
+use std::collections::{HashSet};
 use yaah::aoc;
 use crate::utilities::split_once_and_trim;
 
+#[derive(Debug)]
 struct Card {
+    identifier: u32,
     winning_numbers: HashSet<u8>,
     owned_numbers: HashSet<u8>
 }
 
 #[aoc(day4, part1)]
-fn day4_part1(input: &'static str) -> u32 {
+fn day4_part1(input: &'static str) -> usize {
     input.lines()
-        .map(remove_input_prefix)
-        .map(convert_raw_to_card)
-        .map(calculate_score)
+        .map(parse_into_card)
+        .map(|card| calculate_score(&card))
         .sum()
 }
 
-fn remove_input_prefix(input: &str) -> &str {
-    split_once_and_trim(input, ':')
-        .map(|(_, data)| data)
-        .unwrap()
-}
+fn parse_into_card(input: &str) -> Card {
+    let (raw_day, raw_numbers) =  split_once_and_trim(input, ':').unwrap();
+    let (raw_winning, raw_owned) = split_once_and_trim(raw_numbers, '|').unwrap();
 
-fn convert_raw_to_card(input: &str) -> Card {
-    let (raw_winning, raw_owned) = split_once_and_trim(input, '|').unwrap();
+    let day = raw_day.trim_start_matches("Card").trim().parse().unwrap();
 
     Card {
-        winning_numbers: parse_raw_into_num_array(raw_winning),
-        owned_numbers: parse_raw_into_num_array(raw_owned)
+        identifier: day,
+        winning_numbers: parse_into_num_array(raw_winning),
+        owned_numbers: parse_into_num_array(raw_owned)
     }
 }
 
-fn parse_raw_into_num_array(input: &str) -> HashSet<u8> {
+fn parse_into_num_array(input: &str) -> HashSet<u8> {
     input.split_ascii_whitespace()
         .map(str::parse)
         .map(Result::unwrap)
         .collect()
 }
 
-fn calculate_score(card: Card) -> u32 {
+fn calculate_score(card: &Card) -> usize {
     card.winning_numbers
         .intersection(&card.owned_numbers)
         .fold(0, |acc, _|  if acc == 0 { 1 } else { acc * 2 })
+}
+
+#[aoc(day4, part2)]
+fn day4_part2(input: &'static str) -> u32 {
+    let cards: Vec<Card> = input.lines()
+        .map(parse_into_card)
+        .collect();
+
+    calculate_amount_of_cards(&cards)
+}
+
+fn calculate_amount_of_cards(cards: &[Card]) -> u32 {
+    const fn card_as_index(card: &Card) -> usize { (card.identifier - 1) as usize }
+
+    let mut card_amounts: Vec<u32> = vec![1; cards.len()];
+
+    for card in cards {
+        let card_index = card_as_index(card);
+        let amount_of_card = card_amounts[card_index];
+
+        let score = winning_intersection_size(card);
+        let next_index = card_index + 1;
+
+        for index in next_index..next_index + score {
+            let Some(amount) = card_amounts.get_mut(index) else {
+                continue
+            };
+
+            *amount += amount_of_card;
+        }
+    }
+
+    card_amounts.iter().sum()
+}
+
+fn winning_intersection_size(card: &Card) -> usize {
+    card.winning_numbers
+        .intersection(&card.owned_numbers)
+        .count()
 }
