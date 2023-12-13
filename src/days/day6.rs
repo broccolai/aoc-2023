a8317e3450b4ac7e1f99c65d9539321fda486b73
broccoli <me@broccol.ai>
2023-12-13 15:57:15 +0000

    day(six): part two working

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,9 +1,11 @@
+use std::u64;
+
 use crate::utilities::{parse_vec_of_numbers, remove_before_once_and_trim};
 use yaah::aoc;
 
 struct Race {
-    time: u32,
-    distance: u32,
+    time: u64,
+    distance: u64,
 }
 
 #[aoc(day6, part1)]
@@ -11,7 +13,7 @@ fn day6_part1(input: &'static str) -> usize {
     parse_input(input).iter().map(count_ways_to_win).product()
 }
 
-fn parse_input(input: &'static str) -> Vec<Race> {
+fn parse_input(input: &str) -> Vec<Race> {
     let parsed_lines: Vec<Vec<u32>> = input
         .lines()
         .map(|line| remove_before_once_and_trim(line, ':'))
@@ -20,8 +22,8 @@ fn parse_input(input: &'static str) -> Vec<Race> {
 
     Iterator::zip(parsed_lines[0].iter(), parsed_lines[1].iter())
         .map(|(time, distance)| Race {
-            time: *time,
-            distance: *distance,
+            time: u64::from(*time),
+            distance: u64::from(*distance),
         })
         .collect()
 }
@@ -34,4 +36,24 @@ fn count_ways_to_win(race: &Race) -> usize {
             distance > race.distance
         })
         .count()
+}
+
+#[aoc(day6, part2)]
+fn day6_part2(input: &'static str) -> usize {
+    count_ways_to_win(&parse_input_singular(input))
+}
+
+fn parse_input_singular(input: &str) -> Race {
+    let parsed_lines: Vec<u64> = input
+        .lines()
+        .map(|line| remove_before_once_and_trim(line, ':'))
+        .map(|line| line.replace(' ', ""))
+        .map(|line| line.parse())
+        .map(Result::unwrap)
+        .collect();
+
+    Race {
+        time: parsed_lines[0],
+        distance: parsed_lines[1],
+    }
 }
