4eafdf8f9e7358b31bc53ed3de074b79d77dbaff
broccoli <me@broccol.ai>
2023-12-16 15:27:22 +0000

    day(eight): part two working

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -34,20 +34,9 @@ const TARGET_VALUE: &str = "ZZZ";
 
 #[aoc(day8, part1)]
 fn day8_part1(input: &'static str) -> usize {
-    let Input { pattern, mappings } = parse_input(input);
-
-    return pattern
-        .iter()
-        .cycle()
-        .scan(STARTING_VALUE, |state, instruction| {
-            if *state == TARGET_VALUE {
-                return None;
-            }
-
-            *state = mappings.get(*state).unwrap().from_instruction(instruction);
-            Some(1)
-        })
-        .count();
+    find_steps_for_value(&parse_input(input), STARTING_VALUE, |value| {
+        value == TARGET_VALUE
+    })
 }
 
 fn parse_input(input: &str) -> Input {
@@ -89,4 +78,49 @@ fn parse_mapping(input: &str) -> Mapping {
         left: left.to_string(),
         right: right.to_string(),
     }
+}
+
+fn find_steps_for_value(input: &Input, value: &str, target: fn(&str) -> bool) -> usize {
+    let Input { pattern, mappings } = input;
+
+    return pattern
+        .iter()
+        .cycle()
+        .scan(value, |state, instruction| {
+            if target(state) {
+                return None;
+            }
+
+            *state = mappings.get(*state).unwrap().from_instruction(instruction);
+            Some(1)
+        })
+        .count();
+}
+
+const END: &str = "Z";
+
+#[aoc(day8, part2)]
+fn day8_part2(input: &'static str) -> i64 {
+    let parsed_input = parse_input(input);
+
+    parsed_input
+        .mappings
+        .keys()
+        .filter(|key| key.ends_with("A"))
+        .map(|value| find_steps_for_value(&parsed_input, value, |current| current.ends_with(END)))
+        .map(|number| number as i64)
+        .fold(1i64, lcm)
+}
+
+const fn gcd(mut a: i64, mut b: i64) -> i64 {
+    while b != 0 {
+        let t = b;
+        b = a % b;
+        a = t;
+    }
+    a.abs()
+}
+
+const fn lcm(a: i64, b: i64) -> i64 {
+    a * b / gcd(a, b)
 }
